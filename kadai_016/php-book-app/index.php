d6ebb847066276f7cceebcb91b74aaa0dc0924ae
hisao-matsushita <blackmickey.1303@gmail.com>
2024-07-22 11:12:44 +0900

    Add files via upload

---
+++ kadai_016/php-book-app/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="widht=device-widht, initial-scale-scale=1.0">
+    <tilte>書籍管理アプリ</tilte>
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Google Fontsの読み込み -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <a href="index.php">書籍管理アプリ</a>
+        </nav>
+    </header>
+    <main>
+        <article class="home">
+            <h1>書籍管理アプリ</h1>
+            <p>『phpとデータベースを連携しよう』成果物</p>
+            <a href="read.php" class="btn">書籍一覧</a>
+        </article>
+    </main>
+    <footer>
+        <p class="copyright">&copy;書籍管理アプリ ALL rights reserved.</p>
+    </footer>
+</body>
+
+</html>
